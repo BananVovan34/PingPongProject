12f203003a335036d1b02ea211552a83e2ccac01
BananVovan34 <ivavladimir777@gmail.com>
2025-09-23 18:31:26 +0300

    Init

--- Assets/PingPong/Scripts/Gameplay/Game/GameStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Gameplay.Game
-{
-    public enum GameStatus
-    {
-        WAITING_FOR_PLAYERS,
-        PLAYING
-    }
-}
--- Assets/PingPong/Scripts/Gameplay/Interface/IMoveableController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Gameplay.Interface
-{
-    public interface IMoveableController
-    {
-        public void Move(Vector2 direction);
-    }
-}
