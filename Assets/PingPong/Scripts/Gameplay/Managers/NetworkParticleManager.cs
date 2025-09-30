5565734049a928c7f2ec8a4bc63a4acba72e78cd
BananVovan34 <ivavladimir777@gmail.com>
2025-09-30 12:09:17 +0300

    Goal particles

---
+++ Assets/PingPong/Scripts/Gameplay/Managers/NetworkParticleManager.cs
@@ -0,0 +1,58 @@
+﻿using PingPong.Scripts.Gameplay.Ball;
+using PingPong.Scripts.Gameplay.Particles;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace PingPong.Scripts.Gameplay.Managers
+{
+    public class NetworkParticleManager : NetworkBehaviour
+    {
+        public static NetworkParticleManager Instance { get; private set; }
+
+        [SerializeField] private EmitParticleController leftGoalParticles;
+        [SerializeField] private EmitParticleController rightGoalParticles;
+        
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            if (!IsServer) return;
+
+            BallController.Instance.OnScoreZoneReached += EmitParticles;
+        }
+        
+        public override void OnNetworkDespawn()
+        {
+            if (!IsServer) return;
+
+            BallController.Instance.OnScoreZoneReached -= EmitParticles;
+        }
+
+        private void EmitParticles(string zoneTag)
+        {
+            if (zoneTag == "ScoreZoneLeft")
+                EmitParticlesClientRpc(true);
+            else if (zoneTag == "ScoreZoneRight")
+                EmitParticlesClientRpc(false);
+        }
+
+        [ClientRpc]
+        private void EmitParticlesClientRpc(bool isLeft)
+        {
+            if (isLeft)
+            {
+                leftGoalParticles.EmitRandomValue(16, 32 + 1);
+            }
+            else
+                rightGoalParticles.EmitRandomValue(16, 32 + 1);
+        }
+    }
+}
---
+++ Assets/PingPong/Scripts/Gameplay/Particles/EmitParticleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace PingPong.Scripts.Gameplay.Particles
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class EmitParticleController : MonoBehaviour
+    {
+        private ParticleSystem _particleSystem;
+
+        private void Awake() =>
+            _particleSystem = GetComponent<ParticleSystem>();
+        
+        public void Emit(int value) =>
+            _particleSystem.Emit(value);
+
+        public void EmitRandomValue(int minValue, int maxValue)
+        {
+            int value = UnityEngine.Random.Range(minValue, maxValue);
+            
+            _particleSystem.Emit(value);
+        }
+    }
+}
