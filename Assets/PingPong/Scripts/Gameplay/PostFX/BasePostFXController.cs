39af96674b63c8f1db2aee9b44b61ccb11a4f9e6
BananVovan34 <ivavladimir777@gmail.com>
2025-09-30 14:39:10 +0300

    PostFX controllers

---
+++ Assets/PingPong/Scripts/Gameplay/PostFX/BasePostFXController.cs
@@ -0,0 +1,52 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace PingPong.Scripts.Gameplay.PostFX
+{
+    public abstract class BasePostFXController : NetworkBehaviour
+    {
+        [Header("FX Settings")]
+        [SerializeField] protected AnimationCurve curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1), new Keyframe(0, 0));
+        [SerializeField] protected float effectDuration = 0.35f;
+
+        private float _timer;
+        private bool _isActive;
+
+        private void Update()
+        {
+            if (!_isActive) return;
+
+            _timer -= Time.deltaTime;
+            float normalizedTime = Mathf.Clamp01(1 - (_timer / effectDuration));
+            float curveValue = curve.Evaluate(normalizedTime);
+
+            ApplyEffect(curveValue);
+
+            if (_timer <= 0f)
+                ResetEffect();
+        }
+        
+        protected abstract void ApplyEffect(float curveValue);
+        protected abstract void ResetToDefault();
+
+        [ClientRpc]
+        public void TriggerEffectClientRPC() => TriggerEffect();
+        
+        public void TriggerEffect() => TriggerEffect(effectDuration);
+
+        public void TriggerEffect(float duration)
+        {
+            _timer = duration;
+            _isActive = true;
+        }
+
+        public void StopEffect() => ResetEffect();
+
+        private void ResetEffect()
+        {
+            _timer = 0f;
+            _isActive = false;
+            ResetToDefault();
+        }
+    }
+}
---
+++ Assets/PingPong/Scripts/Gameplay/PostFX/BloomIntensityController.cs
@@ -0,0 +1,52 @@
+﻿using PingPong.Scripts.Gameplay.Ball;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace PingPong.Scripts.Gameplay.PostFX
+{
+    public class BloomIntensityController : BasePostFXController
+    {
+        private Volume _volume;
+        private Bloom _bloom;
+        private float _defaultIntensity;
+
+        private void Awake()
+        {
+            _volume = GetComponent<Volume>();
+            if (_volume == null)
+                throw new System.NullReferenceException("Volume is null");
+
+            if (!_volume.profile.TryGet(out _bloom))
+                throw new System.NullReferenceException("Bloom profile is null");
+
+            _defaultIntensity = _bloom.intensity.value;
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            if (!IsServer) return;
+
+            BallController.Instance.OnScoreZoneReached += TriggerEffect;
+        }
+        
+        public override void OnNetworkDespawn()
+        {
+            if (!IsServer) return;
+
+            BallController.Instance.OnScoreZoneReached -= TriggerEffect;
+        }
+
+        private void TriggerEffect(string obj) =>
+            TriggerEffectClientRPC();
+
+        protected override void ApplyEffect(float curveValue)
+        {
+            _bloom.intensity.value = _defaultIntensity + curveValue;
+        }
+
+        protected override void ResetToDefault()
+        {
+            _bloom.intensity.value = _defaultIntensity;
+        }
+    }
+}
